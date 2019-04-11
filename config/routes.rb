519d87ed1b7e81d1ad647b467961926fa937bd28
Amy Wyatt <amy.wyatt12@outlook.com>
2019-04-10 23:32:15 -0700

    added routes for new and create

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get "root", to: "tasks#index"
 
   get "/tasks/:id", to: "tasks#show", as: "task"
+
+  get "/tasks/new", to: "tasks#new", as: "new_task"
+  post "/tasks", to: "tasks#create"  #, as: "create_task"
 end
